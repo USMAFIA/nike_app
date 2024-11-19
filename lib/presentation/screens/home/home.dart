82233fe2e385ffeab993ad1c987efa47b9d8872a
osama <usmafia302@gmail.com>
2024-11-19 14:44:01 +0500

    2nd commit

--- lib/presentation/screens/home/home.dart
+++ lib/presentation/screens/home/home.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nike_app/constant/colors.dart';
+
+import '../../../constant/global_constants.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,57 +12,72 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final int _page = 0;
+   late int _page = 0;
+   PageController pageController = PageController();
+
+   @override
+   void dispose() {
+     super.dispose();
+     pageController.dispose();
+   }
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  void navigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.w,
-      bottomNavigationBar: BottomNavigationBar(items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(
-            (_page == 0) ?
-            Icons.home : Icons.home_outlined,
-            color: (_page == 0) ? AppColors.w : AppColors.bk,
-          ),
-          label: '',
-          backgroundColor: AppColors.bk,
-        ),
-        BottomNavigationBarItem(
-            icon: Icon(
-              (_page == 1) ?
-              Icons.search : Icons.search_sharp,
-              color: (_page == 1) ? AppColors.w : AppColors.bk,
+      bottomNavigationBar: CupertinoTabBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined,
+              color: (_page == 0) ?AppColors.bk : Colors.grey[600] ,
             ),
-            label: '',
-            backgroundColor: AppColors.bk),
-        BottomNavigationBarItem(
-            icon: Icon(
-              (_page == 2) ?
-              Icons.add_circle : Icons.add_circle_outline,
-              color: (_page == 2) ? AppColors.w : AppColors.bk,
+            label: 'Home',
+            backgroundColor: AppColors.w,
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search_sharp,
+                color: (_page == 1) ?  AppColors.bk : Colors.grey[600],
+              ),
+              label: 'Shop',
+              backgroundColor: AppColors.w),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_outline,
+                color: (_page == 2) ?  AppColors.bk : Colors.grey[600],
+              ),
+              label: 'Favorites',
+              backgroundColor: AppColors.w),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag_outlined,
+              color: (_page == 3) ? AppColors.bk : Colors.grey[600],
             ),
-            label: '',
-            backgroundColor: AppColors.bk),
-        BottomNavigationBarItem(
-          icon: Icon(
-            (_page == 3) ?
-            Icons.video_collection : Icons.video_collection_outlined,
-            color: (_page == 3) ? AppColors.w : AppColors.bk,
+            label: 'Bag',
+            backgroundColor: AppColors.w,
           ),
-          label: '',
-          backgroundColor: AppColors.bk,
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            (_page == 4) ?
-            Icons.person :Icons.person_outline,
-            color: (_page == 4) ? AppColors.w : AppColors.bk,
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline,
+              color: (_page == 4) ? AppColors.bk : Colors.grey[600],
+            ),
+            label: 'Profile',
+            backgroundColor: AppColors.w,
           ),
-          label: '',
-          backgroundColor: AppColors.bk,
-        ),
-      ],),
-      body: Column(),
+        ],
+        onTap: navigationTapped,
+        currentIndex: _page,
+      ),
+      body: PageView(
+        controller: pageController,
+        onPageChanged: onPageChanged,
+        physics:  const NeverScrollableScrollPhysics(),
+        children: GlobalConstants.homeItems(context),
+      ),
     );
   }
 }
